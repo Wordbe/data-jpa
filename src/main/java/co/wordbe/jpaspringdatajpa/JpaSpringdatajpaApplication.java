5a6b1cf382b3611cc16fc742ec66d871ec92d40b
Wordbe <seonghojin3@gmail.com>
2021-06-06 01:21:29 +0900

    init project

---
+++ src/main/java/co/wordbe/jpaspringdatajpa/JpaSpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package co.wordbe.jpaspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringdatajpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpringdatajpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/wordbe/jpaspringdatajpa/JpaSpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.wordbe.jpaspringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringdatajpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
