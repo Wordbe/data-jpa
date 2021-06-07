b634ce73589f7b6e8415d338f4f34ceb2f0b2cb7
Wordbe <seonghojin3@gmail.com>
2021-06-07 22:41:38 +0900

    feat: 쿼리, 파라미터 바인딩, 반환 타입

---
+++ src/main/java/co/wordbe/jpaspringdatajpa/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package co.wordbe.jpaspringdatajpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberDto {
+
+    private Long id;
+    private String username;
+    private String teamName;
+}
--- src/main/java/co/wordbe/jpaspringdatajpa/repository/MemberRepository.java
+++ src/main/java/co/wordbe/jpaspringdatajpa/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package co.wordbe.jpaspringdatajpa.repository;
 
+import co.wordbe.jpaspringdatajpa.dto.MemberDto;
 import co.wordbe.jpaspringdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -33,4 +36,17 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 //    @EntityGraph(attributePaths = {"team"})
     @EntityGraph("Member.all")
     List<Member> findEntityGraphByUsername(@Param("username") String name);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new co.wordbe.jpaspringdatajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    List<Member> findMemberListByUsername(String usernae); // 컬렉션
+    Member findMemberByUsername(String usernae); // 단건
+    Optional<Member> findOptionalMemberByUsername(String usernae); // 단건 옵셔널
 }
